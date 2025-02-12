068514d7c41c57b1b1359d4eaff336d932100684
ArthurMinfotel <arthur.mallet@infotel.com>
2025-02-12 10:51:52 +0100

    migrations for itilchildtemplate targetting

---
+++ install/migrations/update_10.0.x_to_11.0.0/itilfollowuptemplate.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2025 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+/**
+ * @var \DBmysql $DB
+ * @var \Migration $migration
+ * @var array $DELFROMDISPLAYPREF
+ */
+
+$entity_table = Entity_ITILFollowupTemplate::getTable();
+if ($DB->tableExists($entity_table)) {
+    $table = ITILFollowupTemplate::getTable();
+    $query = "INSERT INTO {$entity_table} (itilfollowuptemplates_id, entities_id, is_recursive)
+SELECT id, entities_id, is_recursive
+FROM {$table};";
+    $DB->doQuery($query);
+}
+
